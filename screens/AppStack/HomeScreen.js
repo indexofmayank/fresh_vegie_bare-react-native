8bdb587dcab73d56d419e1fc340a159598f6e1c8
indexofmayank <maimayanmanu@gmail.com>
2024-07-05 00:17:20 +0530

    icons-done

--- screens/AppStack/HomeScreen.js
+++ screens/AppStack/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class HomeScreen extends React.Component {
     render() {
         return (
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
                 <Text>Home screen</Text>
+                <Icon name="location-outline" />
             </View>
         );
     }
